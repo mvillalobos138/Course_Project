10c41eaaa84156f9201a9fc072d031fb76ecf238
Michael Villalobos <mvillalobos103@gmail.com>
2018-11-26 01:03:28 -0500

    completed tic-tac-toe local two player capability. Handles player 1 win, player 2 win and tie games

--- app/src/main/java/com/example/michaelvillalobos/course_project/play_ttt.java
+++ app/src/main/java/com/example/michaelvillalobos/course_project/play_ttt.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 public class play_ttt extends AppCompatActivity {
 
@@ -14,7 +15,17 @@ public class play_ttt extends AppCompatActivity {
     }
     public void PVP_ttt(View view) {
         Intent intent = new Intent(this, TTT_PVP.class);
-        startActivity(intent);
+        startActivityForResult(intent, 1);
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if(resultCode == 1)
+            Toast.makeText(getApplication().getApplicationContext(), "Player 1 Wins!", Toast.LENGTH_LONG).show();
+        else if(resultCode == 2)
+            Toast.makeText(getApplication().getApplicationContext(), "Player 2 Wins!", Toast.LENGTH_LONG).show();
+        else
+            Toast.makeText(getApplication().getApplicationContext(), "Tie Game!", Toast.LENGTH_LONG).show();
+
+    }
 }
